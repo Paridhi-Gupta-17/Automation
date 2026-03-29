2d5caa6145e023dd9e23f1fbb7cbedc9a96460ac
Paridhi <paridhi4567@gmail.com>
2026-03-29 10:21:33 +0530

    Added CI/CD pipeline

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import Pages.LoginPage;
-// changing for push my second changes for jenkins 
+// changing for push my second changes for jenkins pus again 
 
 public class LoginSteps {
 
